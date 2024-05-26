13b48a06e9976e86927651f798bd4ab47bc517c2
Dave <davislyu@gmail.com>
2024-05-26 16:06:36 +0300

    added product modal

---
+++ src/components/ProductModal.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Product Modal</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'</script>
+  import { Button } from "@/components/ui/button";
+</script>
 
 <template>
-<div>
-  Home Page
-  <Button variant="destructive" >Click me</Button>
-</div>
+  <div class="w-full max-w-4xl mx-auto my-10">
+    <div class="flex items-center justify-between">
+      <h3 class="text-2xl font-bold">All Products</h3>
+      <Button>Add Product</Button>
+    </div>
+  </div>
 </template>

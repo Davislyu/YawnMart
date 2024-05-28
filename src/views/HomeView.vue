cd3a25d9a100f7f42c1a8004d94cc7cd927a79bd
Dave <davislyu@gmail.com>
2024-05-28 16:25:35 +0300

    implanted cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+  import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
   import { Button } from "@/components/ui/button";
+  import { Icon } from "@iconify/vue";
   import ProductModal from "@/components/ProductModal.vue";
   import productModal from "@/composables/useProductModal";
   import { useCategoryStore } from "@/stores/categoryStore";
   const { onOpen, isOpen } = productModal();
   const categoryStore = useCategoryStore();
   import { useGlobalLoader } from "vue-global-loader";
-  import { onMounted } from "vue";
+  import { onMounted, computed, ref } from "vue";
   import { useProductStore } from "@/stores/productStore";
   const productStore = useProductStore();
   const { displayLoader, destroyLoader } = useGlobalLoader();
+  const products = computed(() => productStore.productsData.products);
+
+  const totalPages = computed(() => productStore.productsData.totalPages);
+
+  const totalProducts = computed(() => productStore.productsData.totalProducts);
+
+  const productsData = computed(() => productStore.productsData);
+
+  const currentPage = ref(1);
   ////////////////////////
   const fetchCategories = async () => {
     try {
@@ -22,7 +40,7 @@
 
   const fetchProducts = async () => {
     try {
-      await productStore.getProducts(1, 5);
+      await productStore.getProducts(currentPage.value, 5);
     } catch (error) {
       console.log(error);
     }
@@ -50,6 +68,36 @@
     <div class="flex items-center justify-between">
       <h3 class="text-2xl font-bold">All Products</h3>
       <Button @click="onOpen">Add Product</Button>
+    </div>
+    <div class="grid w-full grid-cols-3 gap-4 my-10">
+      <Card
+        v-for="product in products"
+        :key="product._id"
+        class="flex flex-col justify-between">
+        <CardContent class="grid gap-4 p-4 pt-4">
+          <div class="overflow-hidden rounded-md">
+            <img
+              :src="product.mainImage.url"
+              :alt="product.name"
+              class="object-cover w-full h-auto transition-all hover:scale-105" />
+          </div>
+          <h3 class="text-xl font-semibold">
+            {{ product.name }}
+          </h3>
+          <p>{{ product.description }}</p>
+        </CardContent>
+        <CardFooter class="justify-between w-full p-4 pt-4 border-t">
+          <p>{{ product.price }}$</p>
+          <div class="flex space-x-2">
+            <Button variant="secondary">
+              <Icon icon="mdi:pencil-outline" class="w-4 h-4" />
+            </Button>
+            <Button variant="destructive">
+              <Icon icon="ph:trash-bold" class="w-4 h-4" />
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   </div>
 </template>
